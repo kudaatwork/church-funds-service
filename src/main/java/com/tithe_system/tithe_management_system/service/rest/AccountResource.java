2c553a44f66ccba7a9ac17ed207f576f49c15cfc
Kudakwashe Daniel Mundangepfupfu <kuda.mundangepfupfu@econet.co.zw>
2024-08-04 18:35:09 +0200

    Added Payments Multiple Filters

--- src/main/java/com/tithe_system/tithe_management_system/service/rest/AccountResource.java
+++ src/main/java/com/tithe_system/tithe_management_system/service/rest/AccountResource.java
@@ -3,13 +3,16 @@ package com.tithe_system.tithe_management_system.service.rest;
 import com.tithe_system.tithe_management_system.service.processor.api.AccountServiceProcessor;
 import com.tithe_system.tithe_management_system.utils.constants.Constants;
 import com.tithe_system.tithe_management_system.utils.responses.AccountResponse;
+import com.tithe_system.tithe_management_system.utils.responses.AssemblyResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Locale;
 
@@ -31,5 +34,19 @@ public class AccountResource {
                                              defaultValue = Constants.DEFAULT_LOCALE) final Locale locale)
     {
         return accountServiceProcessor.findById(id, locale);
+    }
+
+    @Operation(summary = "Find all accounts as pages")
+    @GetMapping(value = "/pages")
+    public AccountResponse findAllAsPages(@Parameter(name = "Authorization", in = ParameterIn.HEADER,
+            description = "Bearer token", required = true)
+                                           String authenticationToken,
+                                           @RequestParam(value = "page", defaultValue = "0") int page,
+                                           @RequestParam(value = "size", defaultValue = "10") int size,
+                                           @Parameter(description = Constants.LOCALE_LANGUAGE_NARRATIVE)
+                                           @RequestHeader(value = Constants.LOCALE_LANGUAGE,
+                                                   defaultValue = Constants.DEFAULT_LOCALE) final Locale locale)
+    {
+        return accountServiceProcessor.findAllAsPages(page, size, locale, authenticationToken);
     }
 }
